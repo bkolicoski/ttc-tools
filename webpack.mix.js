cfc8af3bba8500a147c240f1fb25f55fc4dacf09
Blagojce Kolicoski <bill@thebrightbox.com>
2019-09-15 05:55:04 +0200

    Added option for purging unused css when building for production

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-
+require('laravel-mix-purgecss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -18,4 +18,4 @@ mix.js('resources/js/app.js', 'public/js')
     postCss: [
       tailwindcss('./tailwind.config.js')
     ],
-});
+}).purgeCss();
