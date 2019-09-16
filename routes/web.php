7578ed37f05c93438a0d12dd2367dbdb6e88c826
Blagojce Kolicoski <bill@thebrightbox.com>
2019-09-16 07:02:34 +0200

    Converted routes closure to controller for welcome page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@welcome');
 
 Route::group(['as' => 'youtube-sight.', 'prefix' => 'youtube-sight'], function () {
     Route::get('/', 'YouTubeSightController@index')->name('index');
