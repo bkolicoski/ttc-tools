d0749aef229a775b3dfdd29492541ad60583b165
Bill Kolicoski <bkolicoski@gmail.com>
2020-02-09 09:47:24 +0100

    Finished adding functionality, fixed responsive display

--- resources/js/components/LinkExtractor.vue
+++ resources/js/components/LinkExtractor.vue
@@ -1,22 +1,49 @@
 <template>
-    <div class="w-11/12 mx-auto mb-5 overflow-auto">
-        <div class="w-full float-left">
-            <button class="block float-right px-2 py-1 border-2 rounded-lg hover:bg-blue-100"><i class="fas fa-copy"></i> Copy All</button>
-        </div>
-        <div class="w-full clearfix flex float-left">
-            <div class="w-1/2 flex-1 p-3">
-                <textarea name="input_text" id="input_text" cols="30" rows="15" class="w-full p-2 rounded-lg" v-model="input_text"></textarea>
+    <div class="w-full lg:w-11/12 mx-auto mb-5 overflow-auto">
+        <div class="w-full float-left pr-3">
+            <button class="block float-right px-2 py-1 border-2 rounded-lg hover:bg-blue-100 active:bg-blue-200 outline-none active:outline-none focus:outline-none" @click="copyAllLinks">
+                <i ref="copyAll" class="fas fa-copy"></i> Copy All
+            </button>
+            <div class="float-right mr-5 pt-2">
+                <label for="match_mode" class="flex items-center cursor-pointer">
+                    <div class="mr-3 text-gray-700 font-medium">
+                        Exact Match
+                    </div>
+                    <div class="relative">
+                        <input id="match_mode" v-model="match_mode" type="checkbox" class="hidden" />
+                        <div class="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
+                        <div class="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
+                    </div>
+                    <div class="ml-3 text-gray-700 font-medium">
+                        Loose Match
+                    </div>
+                </label>
             </div>
-            <div class="w-1/2 flex-1 p-3">
-                <div v-if="links === null" class="w-full pb-1 overflow-auto">
+        </div>
+        <div class="w-full clearfix flex flex-col md:flex-row float-left">
+            <div class="w-full md:w-1/2 flex-1 p-3">
+                <span class="block pb-2">Input the text below to extract links from</span>
+                <textarea
+                    name="input_text"
+                    id="input_text"
+                    cols="30" rows="15"
+                    class="w-full p-2 rounded-lg border-2 border-gray-400 outline-none"
+                    v-model="input_text"
+                    placeholder="Input text with links"
+                ></textarea>
+            </div>
+            <div class="w-full md:w-1/2  flex-1 p-3 mt-8">
+                <div v-if="links === null" class="w-full overflow-auto text-center">
                     There were no links identified in the text on the left.
                 </div>
-                <div v-else class="w-full pb-1 overflow-auto" v-for="link in links">
-                    <a :href="link" :title="link" target="_blank" class="block float-left w-11/12 p-1 bg-blue-100 rounded-lg border-2 truncate">
+                <div v-else class="w-full pb-1 overflow-auto" v-for="(link, index) in links">
+                    <a :href="formatLink(link)" :title="link" target="_blank" class="block float-left w-11/12 p-1 bg-blue-100 rounded-lg border-2 truncate">
                         <span class="mx-1"><i class="fas fa-external-link-alt"></i></span>
-                        {{ link }}
+                        {{ formatLink(link) }}
                     </a>
-                    <span class="block float-right border-2 px-2 py-1 rounded-lg hover:bg-blue-100"><i class="fas fa-copy"></i></span>
+                    <span class="block float-right border-2 px-2 py-1 rounded-lg hover:bg-blue-100 outline-none" @click="copySingleLink(index)">
+                        <i :ref="'copy-icon-'+index" class="fas fa-copy"></i>
+                    </span>
                 </div>
 
             </div>
@@ -29,19 +56,87 @@
     export default {
         data () {
             return {
-                input_text: '',
+                input_text: 'regexr.com\n' +
+                    'http://RegExr.com?2rjl6\n' +
+                    'gskinner.com\n' +
+                    'adobe.com/go/flex\n' +
+                    'gskinner.com/products/spl\n' +
+                    'https://google.com\n' +
+                    'https:google.com\n' +
+                    'www.cool.com.au\n' +
+                    'http://www.cool.com.au\n' +
+                    'http://www.cool.com.au/ersdfs\n' +
+                    'http://www.cool.com.au/ersdfs?dfd=dfgd@s=1\n' +
+                    'http://www.cool.com:81/index.html\n',
+                match_mode: false,
                 links: null
             };
         },
-        /*methods: {
-            handleTextChanged () {
-                console.log('it works');
+        methods: {
+            identifyMatches () {
+                this.links = this.input_text.match(this.match_mode ? looseMatch : exactMatch);
+            },
+            formatLink (link) {
+                if (!link.startsWith('http')) {
+                    link = 'http://' + link;
+                }
+                return link;
+            },
+            copyString (str) {
+                const el = document.createElement('textarea');
+                el.value = str;
+                el.setAttribute('readonly', '');
+                el.style.position = 'absolute';
+                el.style.left = '-9999px';
+                document.body.appendChild(el);
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);
+            },
+            copyAllLinks () {
+                let str = '';
+                if (this.links != null && this.links.length > 0) {
+                    for(let i = 0; i < this.links.length; i++) {
+                        str += this.formatLink(this.links[i]) + '\n';
+                    }
+                }
+                this.copyString(str);
+                const el = this.$refs.copyAll;
+                this.switchFeedbackIcon(el);
+            },
+            copySingleLink (index) {
+                this.copyString(this.links[index]);
+                const el = this.$refs['copy-icon-'+index][0];
+                this.switchFeedbackIcon(el);
+            },
+            switchFeedbackIcon (el) {
+                el.classList.remove("fa-copy");
+                el.classList.add("fa-check");
+                setTimeout(() => {
+                    el.classList.remove("fa-check");
+                    el.classList.add("fa-copy");
+                }, 500);
             }
-        },*/
+        },
         watch: {
-            input_text (value) {
-                this.links = value.match(looseMatch);
+            input_text () {
+                this.identifyMatches();
+            },
+            match_mode () {
+                this.identifyMatches();
             }
         }
     }
 </script>
+<style scoped>
+    .toggle__dot {
+        top: -.25rem;
+        left: -.25rem;
+        transition: all 0.3s ease-in-out;
+    }
+
+    input:checked ~ .toggle__dot {
+        transform: translateX(100%);
+        background-color: #48bb78;
+    }
+</style>
--- resources/views/link-extractor/index.blade.php
+++ resources/views/link-extractor/index.blade.php
@@ -5,8 +5,9 @@
 @endsection
 
 @section('content')
-    <div class="w-full md:w-3/4 mx-auto">
-        <p class="mb-4">Link Extractor </p>
+    <div class="w-full lg:w-11/12  mx-auto">
+        <p class="mb-4 p-3">Link Extractor allows you to extract all of the URL links in any given text. It can work with plain text, HTML or any other text where there are links inside.
+            I've build it to help me extract the links I use for my affiliates on the <a  class="text-blue-400 hover:text-blue-600" href="https://www.youtube.com/tastethecode">channel</a>, where on some, only the full HTML is allowed to be copied. </p>
 
         @if (session('error'))
             <div class="bg-red-200 my-6">
@@ -21,4 +22,14 @@
 
     </div>
     <link-extractor></link-extractor>
+    <p class="mb-4 p-3">This tool is provided free of charge and its code can be found together with the rest of my tools on the <a  class="text-blue-400 hover:text-blue-600" href="https://github.com/bkolicoski/ttc-tools">GitHub repo</a>. However, hosting costs me money and for that, any donation is more than welcomed. </p>
+    <p class="text-center">
+        <button class="px-2 py-1 border-2 rounded-lg hover:bg-blue-100 active:bg-blue-200 outline-none active:outline-none focus:outline-none" onclick="window.location.href = 'https://www.patreon.com/taste_the_code'">
+            <i class="fab fa-patreon"></i> Become a Patron
+        </button>
+        <button class="px-2 py-1 border-2 rounded-lg hover:bg-blue-100 active:bg-blue-200 outline-none active:outline-none focus:outline-none" onclick="window.location.href = 'https://www.paypal.com/paypalme2/bkolicoski'">
+            <i class="fas fa-beer"></i> Buy me a beer
+        </button>
+    </p>
+
 @stop
